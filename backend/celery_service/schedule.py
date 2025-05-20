055fa7688895602517430ad19ff04e10d3d460f8
Roman <rvg95@mail.ru>
2025-05-20 15:04:13 +0500

    Изменен график задачи 'update-delivery-statuses' в Celery: заменен на выполнение каждые 2 часа вместо каждую минуту. Это оптимизирует нагрузку на систему и улучшает производительность.

--- backend/celery_service/schedule.py
+++ backend/celery_service/schedule.py
@@ -8,7 +8,8 @@ beat_schedule = {
      'update-delivery-statuses': {
          'task': 'order.update_boxberry_statuses',
          #'schedule': crontab(hour=19, minute=0),  # 19:00 UTC = 00:00 Екатеринбург (UTC+5)
-         'schedule': crontab(minute='*'), 
+         #'schedule': crontab(minute='*'), 
+         'schedule': crontab(minute=0, hour='*/2'),
          #'args': (1,),  # Аргумент: дни для сохранения корзин
          #'options': {'queue': 'cart'},  # Указываем очередь
      },
